3e182400f1bbd2e76ca875c797e927703c4a5b7f
Matteo <116072651+rimaout@users.noreply.github.com>
2026-04-22 23:36:20 +0200

    nvim: Add mini.nvim plugin and enable modules
    Add new Neovim plugin config to include nvim-mini via vim.pack.add and enable several modules with default setups: mini.pairs, mini.notify, mini.indentscope, and mini.cursorword. This brings lightweight pairs, notifications, indent-scope highlighting, and cursor-word highlighting to the configuration.

---
+++ nvim12/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,6 @@
+vim.pack.add({"https://github.com/nvim-mini/mini.nvim"})
+
+require("mini.pairs").setup({})
+require("mini.notify").setup({})
+require("mini.indentscope").setup({})
+require("mini.cursorword").setup({})
