6fc614a0f95183991f7ac4fa7eb0fd5847510ccc
Matteo <116072651+rimaout@users.noreply.github.com>
2026-04-22 23:38:44 +0200

    nvim: Require autocmds and add new plugins
    Replace require('functions') with require('autocmds') and add new plugin entries (plugins.mini, plugins.copilot, plugins.undotree) in init.lua. Update completions.lua: pin blink.cmp to v1.6.0, ensure LuaSnip and friendly-snippets are loaded, and reorganize blink.cmp config (enable signature, set super-tab keymap, nerd font appearance, add default sources, configure completion documentation auto-show and menu draw, keep fuzzy impl). In telescope.lua, comment out the resume mapping.

--- nvim12/.config/nvim/lua/plugins/completions.lua
+++ nvim12/.config/nvim/lua/plugins/completions.lua
@@ -1,33 +1,38 @@
 vim.pack.add({
-	{ src = "https://github.com/Saghen/blink.cmp" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
+	{ src = "https://github.com/Saghen/blink.cmp", version = "v1.6.0" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
 require("blink.cmp").setup({
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
+	-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+	-- 'super-tab' for mappings similar to vscode (tab to accept)
+	-- 'enter' for enter to accept
+	-- 'none' for no mappings
+	--
+	-- All presets have the following mappings:
+	--   C-space: Open menu or open docs if already open
+	--   C-n/C-p or Up/Down: Select next/previous item
+	--   C-e: Hide menu
+	--   C-k: Toggle signature help (if signature.enabled = true)
 
-    keymap = { preset = "super-tab" },
-    appearance = { nerd_font_variant = "mono" },
-	completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
+	keymap = { preset = "super-tab" },
+	appearance = { nerd_font_variant = "mono" },
     signature = { enabled = true },
+
+    sources = { default = { "lsp", "path", "buffer", "snippets"} },
+
     fuzzy = { implementation = "prefer_rust_with_warning" },
 
-	menu = {
-		auto_show = true,
-		draw = {
-			treesitter = { "lsp" },
-			columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
-        }
+    completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        menu = {
+		    auto_show = true,
+		    draw = {
+			    treesitter = { "lsp" },
+			    columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+		    },
+	    }
     }
 })
--- nvim12/.config/nvim/lua/plugins/telescope.lua
+++ nvim12/.config/nvim/lua/plugins/telescope.lua
@@ -23,7 +23,7 @@ vim.keymap.set('n', '<leader>fb',       builtin.builtin,     { desc = '[F]ind Te
 vim.keymap.set('n', '<leader>fw',       builtin.grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fg',       builtin.live_grep,   { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd',       builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-vim.keymap.set('n', '<leader>fr',       builtin.resume,      { desc = '[F]ind [R]esume' })
+--vim.keymap.set('n', '<leader>fr',       builtin.resume,      { desc = '[F]ind [R]esume' })
 vim.keymap.set('n', '<leader>fo',       builtin.oldfiles,    { desc = '[F]ind Olf Files' })
 vim.keymap.set('n', '<leader><leader>', builtin.buffers,     { desc = '[ ] Find existing buffers' })
 
