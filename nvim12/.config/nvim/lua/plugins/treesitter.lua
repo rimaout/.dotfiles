7ed45dac90af6134082a9d647b4883b06d0a0094
Matteo <116072651+rimaout@users.noreply.github.com>
2026-04-22 23:36:40 +0200

    nvim: migrate to tree sitter main branch

--- nvim12/.config/nvim/lua/plugins/treesitter.lua
+++ nvim12/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,34 @@
-vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
+vim.pack.add({'https://github.com/romus204/tree-sitter-manager.nvim'})
 
-require("nvim-treesitter.configs").setup({
-    auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
+-- Remember to install "tree-sitter-cli" (on mac os do "brew install tree-sitter-cli")
 
-    -- incremental_selection (press <Enter> to select a node, <Backspace> to unselect)
-	incremental_selection = {
-	enable = true,
-	keymaps = {
-	    init_selection = "<Enter>",
-        node_incremental = "<Enter>",
-		scope_incremental = false,  -- set to `false` to disable one of the mappings 
-		node_decremental = "<Backspace>",
-		},
-	},
+require("tree-sitter-manager").setup({
+    auto_install = true,    -- if enabled, install missing parsers when editing a new file
+    highlight = true,       -- treesitter highlighting is enabled by default
+
+    ensure_installed = {
+        -- Systems, Compiled & Backend
+        'c', 'cpp', 'rust', 'zig', 'go', 'java', 'swift', 'haskell', 'cuda', 'arduino',
+
+        -- Scripting, Automation & Shell
+        'python', 'ruby', 'lua', 'bash', 'fish',
+
+        -- Web Development (Frontend)
+        'html', 'css', 'javascript', 'typescript', 'vue',
+
+        -- Data Formats & Configuration
+        'json', 'yaml', 'xml', 'csv',
+
+        -- Documentation & Typography
+        'markdown', 'markdown_inline', 'typst',
+
+        -- Git (Integration & Logs)
+        'git_config', 'git_rebase', 'gitcommit', 'gitignore',
+
+        -- Data Analysis & Database
+        'sql', 'r',
+
+        -- Build Tools
+        'cmake'
+    }
 })
