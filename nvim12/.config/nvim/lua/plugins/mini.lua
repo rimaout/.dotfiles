e5a03b69bbe42b79cabc0ab3bc4832dd8cdbf73f
Matteo <116072651+rimaout@users.noreply.github.com>
2026-04-29 21:06:19 +0200

    Add mini plugins & empty space trim keymap
    Enable and configure several nvim-mini plugins: mini.pairs, mini.notify, mini.indentscope, mini.cursorword, mini.surround, mini.comment, and mini.trailspace. Adds a normal-mode mapping <leader>ts to run MiniTrailspace.trim() for trimming trailing whitespace. Also update .gitignore to ignore ghostty/.config/ghostty/.config.un~.

--- nvim12/.config/nvim/lua/plugins/mini.lua
+++ nvim12/.config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,16 @@
 vim.pack.add({"https://github.com/nvim-mini/mini.nvim"})
 
+-- Background Plugin (no actions, this have no mapping)
 require("mini.pairs").setup({})
 require("mini.notify").setup({})
 require("mini.indentscope").setup({})
 require("mini.cursorword").setup({})
+
+-- Action Plugins (with mappings)
+require("mini.surround").setup() -- Add/change/delete surrounding delimiter pairs use 
+require('mini.comment').setup()
+require('mini.trailspace').setup()
+
+vim.keymap.set('n', '<leader>ts', function()
+	MiniTrailspace.trim()
+end, { desc = '[T]rim trailing white[S]pace' })
