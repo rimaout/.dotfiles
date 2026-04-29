17c9c19ce394a9e10e5684fe80e9b653b6eb6583
Matteo <116072651+rimaout@users.noreply.github.com>
2026-04-29 21:08:04 +0200

    Add toggleterm plugin and terminal mappings
    Register and configure akinsho/toggleterm.nvim, enable floating terminals and persistent mode/size. Adds a new toggleterm.lua plugin file that defines two Terminal instances (Shell using fish and GitUI) and maps <leader>t and <leader>g to toggle them. Also adds a global terminal close mapping (Ctrl-Q in terminal mode) and requires the plugin in init.lua.

---
+++ nvim12/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,38 @@
+vim.pack.add({ 'https://github.com/akinsho/toggleterm.nvim' })
+require('toggleterm').setup({ 
+    direction = 'float',
+	start_in_insert = true,
+	persist_mode = true,
+	persist_size = true,
+})
+
+-- Terminal 1: Shell
+local Terminal = require('toggleterm.terminal').Terminal
+local shell_term = Terminal:new({
+	cmd = 'fish',
+	display_name = 'Shell',
+	close_on_exit = false,
+})
+
+function Shell_toggle()
+	shell_term:toggle()
+end
+
+-- Terminal 2: GitUI
+local gitui_term = Terminal:new({
+	cmd = 'gitui',
+	display_name = 'GitUI',
+})
+
+function Gitui_toggle()
+	gitui_term:toggle()
+end
+
+-- Keymaps
+vim.keymap.set('n', '<leader>t', Shell_toggle, { noremap = true, silent = true, desc = 'toggle [T]erminal shell' })
+vim.keymap.set('n', '<leader>g', Gitui_toggle, { noremap = true, silent = true, desc = 'toggle [G]itUI terminal' })
+
+-- Universal close: press Ctrl-Q in terminal mode to close any terminal
+vim.keymap.set('t', '<C-q>', function()
+	require('toggleterm').toggle_all()
+end, { noremap = true, silent = true, desc = 'Close all terminals' })
